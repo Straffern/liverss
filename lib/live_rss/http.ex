e8386eb56a0ae8ed20678c8b5b0b3ec6605b8dd3
Alexander Flensborg <14233825+Straffern@users.noreply.github.com>
2024-07-17 21:24:16 +0200

    Removed explicit try block

--- lib/live_rss/http.ex
+++ lib/live_rss/http.ex
@@ -11,21 +11,19 @@ defmodule LiveRSS.HTTP do
   logs the error returning :error.
   """
   def get(feed_url) do
-    try do
-      with {:ok, {{_, status, _}, _headers, body}} <-
-             :httpc.request(:get, {feed_url, []}, [ssl: :httpc.ssl_verify_host_options(true)], []),
-           status when status in 200..299 <- status,
-           {:ok, %{} = feed} <- FastRSS.parse_rss(IO.iodata_to_binary(body)) do
-        {:ok, feed}
-      else
-        error ->
-          Logger.error("LiveRSS: failed to get feed. Reason: #{inspect(error)}")
-          :error
-      end
-    rescue
-      ArgumentError ->
-        Logger.error("LiveRSS: failed to get feed. Reason: Not XML")
+    with {:ok, {{_, status, _}, _headers, body}} <-
+           :httpc.request(:get, {feed_url, []}, [ssl: :httpc.ssl_verify_host_options(true)], []),
+         status when status in 200..299 <- status,
+         {:ok, %{} = feed} <- FastRSS.parse_rss(IO.iodata_to_binary(body)) do
+      {:ok, feed}
+    else
+      error ->
+        Logger.error("LiveRSS: failed to get feed. Reason: #{inspect(error)}")
         :error
     end
+  catch
+    ArgumentError ->
+      Logger.error("LiveRSS: failed to get feed. Reason: Not XML")
+      :error
   end
 end
